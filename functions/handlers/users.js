2e49d2d3a48ae7b17050a269d4e298c742d648dc
Cory Bass <cory.bass.07@gmail.com>
2020-11-30 00:55:33 -0600

    added function to get user details

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -87,3 +87,40 @@ exports.login = (req, res) => {
         .json({ general: "Wrong credentials, please try again" });
     });
 };
+
+exports.getUserDetails = (req, res) => {
+  let userData = {};
+  db.doc(`/users/${req.params.userName}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        userData.user = doc.data();
+        return db
+          .collection("post")
+          .where("userHandle", "==", req.params.handle)
+          .orderBy("createdAt", "desc")
+          .get();
+      } else {
+        return res.status(404).json({ error: "User not found" });
+      }
+    })
+    .then((data) => {
+      userData.status = [];
+      data.forEach((doc) => {
+        userData.status.push({
+          body: doc.data().body,
+          createdAt: doc.data().createdAt,
+          userName: doc.data().userName,
+          imageUrl: doc.data().imageUrl,
+          likeCount: doc.data().likeCount,
+          commentCount: doc.data().commentCount,
+          postId: doc.id,
+        });
+      });
+      return res.json(userData);
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    });
+};
