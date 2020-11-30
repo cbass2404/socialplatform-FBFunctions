2e294fc2d439c1d4d62222c598e46d8b3730a0c2
Cory Bass <cory.bass.07@gmail.com>
2020-11-30 01:57:11 -0600

    added function to get one post

--- functions/handlers/posts.js
+++ functions/handlers/posts.js
@@ -25,3 +25,31 @@ exports.newPost = (req, res) => {
       console.error(err);
     });
 };
+
+exports.getPost = (req, res) => {
+  let postData = {};
+  db.doc(`/posts/${req.params.postId}`)
+    .get()
+    .then((doc) => {
+      !doc.exists && res.status(404).json({ error: "Post not found" });
+
+      postData = doc.data();
+      postData.postId = doc.id;
+      return db
+        .collection("comments")
+        .orderBy("createdAt", "desc")
+        .where("postId", "==", req.params.postId)
+        .get();
+    })
+    .then((data) => {
+      postData.comments = [];
+      data.forEach((doc) => {
+        postData.comments.push(doc.data());
+      });
+      return res.json(postData);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ error: err.code });
+    });
+};
