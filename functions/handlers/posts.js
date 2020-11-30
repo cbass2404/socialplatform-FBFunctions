8206556a278f5b1cb4716470ae86723e778600a8
Cory Bass <cory.bass.07@gmail.com>
2020-11-30 02:15:56 -0600

    added function to like a post

--- functions/handlers/posts.js
+++ functions/handlers/posts.js
@@ -109,3 +109,50 @@ exports.commentOnPost = (req, res) => {
       res.status(500).json({ error: "Something went wrong" });
     });
 };
+
+exports.likePost = (req, res) => {
+  const likeDocument = db
+    .collection("likes")
+    .where("userName", "==", req.user.userName)
+    .where("postId", "==", req.params.postId)
+    .limit(1);
+
+  const postDocument = db.doc(`/posts/${req.params.postId}`);
+
+  let postData;
+
+  postDocument
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        postData = doc.data();
+        postData.postId = doc.id;
+        return likeDocument.get();
+      } else {
+        return res.status(404).json({ error: "Post not found" });
+      }
+    })
+    .then((data) => {
+      if (data.empty) {
+        return db
+          .collection("likes")
+          .add({
+            postId: req.params.postId,
+            userName: req.user.userName,
+          })
+          .then(() => {
+            postData.likeCount++;
+            return postDocument.update({ likeCount: postData.likeCount });
+          })
+          .then(() => {
+            return res.json(postData);
+          });
+      } else {
+        return res.status(400).json({ error: "Post already liked" });
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ error: err.code });
+    });
+};
