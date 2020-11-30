d6a3cc8ebed34ee7fe9f56e11f45ee088ffe8723
Cory Bass <cory.bass.07@gmail.com>
2020-11-30 01:30:41 -0600

    added function to upload new images

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -187,3 +187,56 @@ exports.addUserDetails = (req, res) => {
       return res.status(500).json({ error: err.code });
     });
 };
+
+exports.uploadImage = (req, res) => {
+  const BusBoy = require("busboy");
+  const path = require("path");
+  const os = require("os");
+  const fs = require("fs");
+
+  const busboy = new BusBoy({ headers: req.headers });
+
+  let newImage = {};
+  let imageName;
+  let generatedToken = uuid();
+
+  busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
+    console.log(fieldname, file, filename, encoding, mimetype);
+    mimetype !== "image/jpeg" &&
+      mimetype !== "image/png" &&
+      res.status(400).json({ error: "Wrong file type submitted" });
+    const imageExtension = filename.split(".")[filename.split(".").length - 1];
+    imageName = `${Math.round(
+      Math.random() * 1000000000000
+    ).toString()}.${imageExtension}`;
+    const filepath = path.join(os.tmpdir(), imageName);
+    newImage = { filepath, mimetype };
+    file.pipe(fs.createWriteStream(filepath));
+  });
+  busboy.on("finish", () => {
+    admin
+      .storage()
+      .bucket()
+      .upload(newImage.filepath, {
+        resumable: false,
+        metadata: {
+          metadata: {
+            contentType: newImage.mimetype,
+            firebaseStorageDownloadTokens: generatedToken,
+          },
+        },
+      })
+      .then(() => {
+        const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFileName}?alt=media&token=${generatedToken}`;
+        return db.doc(`/users/${req.user.userName}`).update({ imageUrl });
+      })
+      .then(() => {
+        return res.json({ message: "Image uploaded." });
+      })
+      .catch((err) => {
+        console.error(err);
+        return res.status(500).json({ error: "Something went wrong." });
+      });
+  });
+  busboy.end(req.rawBody);
+};
